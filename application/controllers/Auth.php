1895039e5deaf629d263077cd55a1eca70e374fe
sergey <s.popov@digital-spectr.ru>
2020-09-13 18:15:57 +0300

    init

---
+++ application/controllers/Auth.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function sigin_in()
+	{
+        $data = [];
+	    if($this->input->post()) {
+            $login = $this->input->post('login');
+            $pass = $this->input->post('password');
+            if($login == 'admin' && $pass == 'password'){
+                $data['success_login'] = true;
+                setcookie('auth' , md5($login));
+            } else {
+                $data['success_login'] = false;
+            }
+        }
+        $this->load->helper('form');
+		$this->load->view('sigin_in' , $data);
+	}
+}
